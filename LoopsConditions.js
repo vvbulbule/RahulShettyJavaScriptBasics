795f50aaa93d362ed8be024dff22e5c08ba47cae
V12bulbule@ <vvbulbule>
2026-01-21 15:01:10 +0530

    File Modified Commit

--- LoopsConditions.js
+++ LoopsConditions.js
@@ -1,3 +1,4 @@
+console.log("Output of if else ")
 const flag= true
 
 if(!flag)
@@ -7,11 +8,11 @@ if(!flag)
 }
 else
 {
-    console.log(flag)
+    console.log(flag)//true
     console.log("Condition Not Satisfied")//Condition Not Satisfied
 }
 
-
+console.log("Output of While Loop")
 // While loop will run unless the condition becomes false
 let a=0
 while(a<10){
@@ -22,9 +23,14 @@ while(a<10){
 //It will execute the loop without chceking the condition 
 // it will execute untill condition becomes false
 // Make sure to end the do while loop with semi colon
+console.log("Output of Do While")
 do{
     a++ 
 }while(a<10);
    console.log(a)
 
-//For Loop
+console.log("Output of Foor Loop ")
+//For Loop print 0 to 10
+for(let k=0;k<=10;k++){
+    console.log(k)
+}
