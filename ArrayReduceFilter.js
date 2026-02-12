6ad01bfa756ab92057ba1a803bd94b0039b886f1
V12bulbule@ <vvbulbule>
2026-02-12 18:08:56 +0530

    Filter code Arrays

--- ArrayReduceFilter.js
+++ ArrayReduceFilter.js
@@ -1,7 +1,7 @@
 let MainArray =[50,60,70,80,90,100]
 
 //To Print all elemts from Array
-//MainArray.length use the size of the array and idex number starts from 0
+//MainArray.length use the size of the array and index number starts from 0
 /* output of For Loop
 50
 60
@@ -31,6 +31,45 @@ console.log(sum)//450
 // sum value is Changing for every Iteration i.e it storing the sum of every iteration
 // sum value is defined at end 
 //mark is the new value comes from array for every iteration
-//reduce will iterate the array like fore loop
-let Addition = MainArray.reduce((sum,mark)=>sum+mark,0)
-console.log(Addition)
+//reduce will iterate the array like for loop
+let Addition = MainArray.reduce((total,num)=>total+num,0)
+console.log(Addition)//450
+
+//To Print the Values multiples of 2
+console.log("To Print the Values multiples of 2 i.e Even Number - O/P") 
+var score=[12,13,14,15]
+
+
+// Using For loop
+evenScore =[]
+
+for (let i=0;i<score.length;i++)
+{
+    if (score[i] % 2 == 0){
+        evenScore.push(score[i])
+        
+    }
+} 
+console.log(evenScore) // 12 14
+
+
+//to find even number simple way is using Filter
+/*Loops through each element of the array
+
+Applies a condition
+
+Returns a new array
+
+Includes only elements where condition is TRUE
+
+For each number (num) in score
+ Check if num % 2 === 0
+If TRUE → keep it
+If FALSE → discard it
+*/
+
+
+evenScore = score.filter(num => num % 2 === 0)
+console.log(evenScore) // [12, 14 ]
+
+
