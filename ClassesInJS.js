b73d3a78e7f93409b81cb0ce1dc7a84ce92b3db4
V12bulbule@ <vvbulbule>
2026-02-17 17:42:45 +0530

    Latest

---
+++ ClassesInJS.js
@@ -0,0 +1,8 @@
+/*Classes in JavaScript are used to create objects in a clean and structured way (introduced in ES6).
+🔥 What is a Class?
+
+A class is a blueprint to create objects.
+
+Instead of writing object manually every time, we create a class and then create multiple objects from it.
+*/
+
