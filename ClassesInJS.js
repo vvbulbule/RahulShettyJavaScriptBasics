6dacee0e7a35aad13e62ceb56ed2850fc35a00d7
V12bulbule@ <vvbulbule>
2026-02-18 17:49:29 +0530

    Class Code Push

--- ClassesInJS.js
+++ ClassesInJS.js
@@ -6,3 +6,41 @@ A class is a blueprint to create objects.
 Instead of writing object manually every time, we create a class and then create multiple objects from it.
 */
 
+class Person{
+    //Class Variables
+    age =25
+
+    // getter method is considered as Property of class 
+    // Property getters
+    get location(){
+        return "Canada"
+    }
+
+    //Constuctor is a method called Aytomatically when when object is created.
+    // Constuctor is Used to initialize values
+    // Here firstName and LastName is instance Variables
+    // Value of the instance variables provided at the runtime while creating the object creation
+    //Scope to access the values of these instance variable is inside of the constructor block only 
+    // To get the access of instance variables outside of the constructoe we use this keyword
+    constructor (firstName,lastName){
+
+        //Now after adding this keywords it Refers to the current class object.
+        // Now firstname and Lastname is accessible to whole class Person
+        this.firstName = firstName
+        this.lastName=lastName
+
+    }
+
+    //Methods
+    fullname (){
+        console.log( this.firstName+" "+ this.lastName)
+    }
+}
+
+//To access the properties of a class like location, Age we have to craete object of the class 
+// object creation of the class done outside of the class
+// Vikrant & Bulbule Values whill be pass to the constructor
+let personDetails= new Person("Vikrant","Bulbule")
+console.log(personDetails.age)//25
+console.log(personDetails.location)//Canada i.e. here we don't have to give () after location because we used the get method for this
+console.log(personDetails.fullname())//Vikrant Bulbule
